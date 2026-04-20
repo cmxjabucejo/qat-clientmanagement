b6c898a8a90c0bf26ffae412b69a0e881e42fdd5
Aerol Unay <a.unaY@callmaxsolutions.com>
2026-04-19 22:03:04 -0400

    update 2

--- frontend/src/components/lib/constants.js
+++ frontend/src/components/lib/constants.js
@@ -1,3 +1,2 @@
-export const SERVER_URL = "http://localhost:5005";
-//export const SERVER_URL = window.location.origin;
-//export const SERVER_URL = "https://cmsportalbackend.cmxph.com";
+//export const SERVER_URL = "http://localhost:5005";
+export const SERVER_URL = window.location.origin;
